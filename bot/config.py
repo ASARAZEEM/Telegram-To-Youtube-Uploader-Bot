f9f20212f31d9d5433e7871e54cbe17d43a9fdde
ASARAZEEM <107904574+ASARAZEEM@users.noreply.github.com>
2023-07-26 13:52:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,27 +2,27 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6434834802:AAEDaDQmWPfN_GTcnXXza2zOg1ACcdNV3FU"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "Goal to 3A YouTube Bot"
 
-    API_ID = ""
+    API_ID = "29425744"
 
-    API_HASH = ""
+    API_HASH = "5c2d2e8d62c3d0728d70e6a1002db718"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "1002979320088-ibuqvqkluspldnej9aqe3fj7m18l2696.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5958287337]
 
     VIDEO_DESCRIPTION = ""
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "Hi"
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "Last"
     
     DEBUG = bool()
 
